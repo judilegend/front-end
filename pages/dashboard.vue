bcda526ab90805278b70aa49da586c0db53dc81a
judilegend <judilegend2.com>
2024-08-07 10:22:06 +0300

    fix-bug/slot

--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -1,23 +1,14 @@
 <script setup lang="ts">
+import AddTaskTemplate from "~/components/AddTaskTemplate.vue";
+</script>
+
+<!-- <script setup lang="ts">
 import Sidebar from "~/components/Sidebar.vue";
 import Navbar from "~/components/Navbar.vue";
 import AddTaskTemplate from "~/components/AddTaskTemplate.vue";
-</script>
+</script> -->
 <template>
-  <main
-    class="w-full grid grid-cols-[300px,1fr] grid-rows-[70px,1fr] h-screen overflow-x-hidden relative"
-  >
-    <div className="row-span-2  ">
-      <Sidebar />
-    </div>
-    <div className="col-span-1 ">
-      <Navbar />
-    </div>
-    <div className="col-start-2 row-start-2 px-8 z-100">
-      <section className="flex w-full justify-between relative">
-        <slot></slot>
-        <!-- <AddTaskTemplate /> -->
-      </section>
-    </div>
-  </main>
+  <div>
+    <AddTaskTemplate />
+  </div>
 </template>
