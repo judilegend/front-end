d3970d1d10c9b161c496d4e39f0984a161ea8ebf
judilegend <judilegend2.com>
2024-08-07 10:03:05 +0300

    add/dash-layout

---
+++ components/AddTaskTemplate.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="flex flex-col justify-center h-screen">
+    <div class="w-full max-w-md bg-white rounded-lg shadow-md p-6">
+      <form class="flex flex-col">
+        <input
+          placeholder="First Name"
+          class="bg-gray-100 text-gray-900 border-0 rounded-md p-2 mb-4 focus:bg-gray-200 focus:outline-none focus:ring-1 focus:ring-blue-500 transition ease-in-out duration-150"
+          type="text"
+        />
+        <input
+          placeholder="Last Name"
+          class="bg-gray-100 text-gray-900 border-0 rounded-md p-2 mb-4 focus:bg-gray-200 focus:outline-none focus:ring-1 focus:ring-blue-500 transition ease-in-out duration-150"
+          type="text"
+        />
+        <input
+          placeholder="Email"
+          class="bg-gray-100 text-gray-900 border-0 rounded-md p-2 mb-4 focus:bg-gray-200 focus:outline-none focus:ring-1 focus:ring-blue-500 transition ease-in-out duration-150"
+          type="email"
+        />
+        <input
+          placeholder="Confirm Email"
+          class="bg-gray-100 text-gray-900 border-0 rounded-md p-2 mb-4 focus:bg-gray-200 focus:outline-none focus:ring-1 focus:ring-blue-500 transition ease-in-out duration-150"
+          type="email"
+        />
+        <input
+          placeholder="Password"
+          class="bg-gray-100 text-gray-900 border-0 rounded-md p-2 mb-4 focus:bg-gray-200 focus:outline-none focus:ring-1 focus:ring-blue-500 transition ease-in-out duration-150"
+          type="password"
+        />
+        <input
+          placeholder="Confirm Password"
+          class="bg-gray-100 text-gray-900 border-0 rounded-md p-2 mb-4 focus:bg-gray-200 focus:outline-none focus:ring-1 focus:ring-blue-500 transition ease-in-out duration-150"
+          type="password"
+        />
+
+        <button
+          class="bg-gradient-to-r from-indigo-500 to-blue-500 text-white font-bold py-2 px-4 rounded-md mt-4 hover:bg-indigo-600 hover:to-blue-600 transition ease-in-out duration-150"
+          type="submit"
+        >
+          Sign Up
+        </button>
+      </form>
+    </div>
+  </div>
+</template>
