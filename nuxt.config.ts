f28c79e1b915449b975c1db425362daffea0e765
judilegend <judilegend2.com>
2024-08-07 06:34:41 +0300

    add-config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
-})
+  modules: ["@pinia/nuxt", "@nuxt-alt/auth"],
+  compatibilityDate: "2024-04-03",
+  devtools: { enabled: true },
+  css: ["~/assets/css/main.css"],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+  auth: {
+    /* module options */
+  },
+});
